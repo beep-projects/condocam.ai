b0b1e71768b0e90931425afcd920d87a2f620d66
beep-projects <beep@freaklamarsch.de>
2023-06-03 20:58:22 +0200

    increased delay for working on images to 0.2

--- condocam-pi/files/condocam_image_watchdog.py
+++ condocam-pi/files/condocam_image_watchdog.py
@@ -33,7 +33,6 @@ from watchdog.observers import Observer
 from watchdog.events import FileSystemEventHandler
 import filetype
 from multiprocessing import Pool
-from multiprocessing import Lock
 import sys
 import multiprocessing
 import subprocess
@@ -41,7 +40,6 @@ import subprocess
 # globals
 BOT_TOKEN = None
 CHAT_ID = None
-LOCK = None
 
 class ImageDirectoryWatcher:  # pylint: disable=too-few-public-methods
   """Watchdog for a specified image directory.
@@ -133,18 +131,14 @@ def process_image(image_queue, confidence):
   while True:
     # check if there is an image in the queue and process it
     try:
-      # the synchronization on the queue seems not to work, so use lock to
-      # prevent two processes working on the same image
-      LOCK.acquire()
       image_path = image_queue.get()
-      LOCK.release()
     except queue.Empty:
       # if queue is empty, wait a while before the next iteration is done
       time.sleep(1)
       continue
     # if the image is not fully written, cv2.imread() returnes None,
     # so give the system some time to finish
-    time.sleep(0.1)
+    time.sleep(0.2)
     # load the input image and construct an input blob for the image
     # by resizing to fit the blob_size and then normalizing it
     # (note: normalization is done via the authors of the MobileNet SSD
@@ -180,12 +174,6 @@ def process_image(image_queue, confidence):
     if person_detected:
       subprocess.run(["telegram.bot", "-bt", BOT_TOKEN, "-cid", CHAT_ID,
                                       "--quiet", "--photo", image_path], check=False)
-
-def init_pool_processes(global_lock):
-  """Initialize each process with a global variable lock.
-  """
-  global LOCK
-  LOCK = global_lock
 
 def main():
   """main function, starts the image directory watchdog, the image processing process pool
@@ -228,9 +216,7 @@ def main():
   pool_size = multiprocessing.cpu_count()
   if pool_size > 1:
     pool_size -= 1
-  # initialize the lock used for image_queue access and pass it to each process via the initializer
-  lock = Lock()
-  with Pool(pool_size, initializer=init_pool_processes, initargs=(lock,)) as pool:
+  with Pool(pool_size) as pool:
     arguments = []
     for _ in range(pool_size):
       arguments.append((image_queue, confidence))
